2865c1dbc776f8512e5eee9690f0592230823392
Nestor <loghim@amazon.com>
2022-06-29 21:48:44 +0300

    liste

---
+++ Saptamana1/structuri_de_date.py
@@ -0,0 +1,21 @@
+#structuri de date
+my_list =[8, 2, 'a',8, 4]
+#print('Mesaj')
+#print(type(my_list))      #tipul unei variabile
+#print(my_list[2])
+#print(my_list[3])
+#print(my_list[-1])
+#print(len(my_list))
+my_list.append('ANA')
+#print(my_list)
+#print(my_list.index('a'))
+my_list.insert(7, 'A')
+#print(my_list[2:5])
+my_list.append(['A', 'B', 3, 'x', [4, 7, 'w']])
+print(my_list[7][4][2])
+#print(my_list[2:])
+#print(my_list[2::2])
+#print(my_list[2::3])
+#print(my_list[7:8])
+print(len(my_list))
+print(my_list[len(my_list) - 1])
