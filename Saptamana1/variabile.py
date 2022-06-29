e9e52fa96b4a3e8f3661724b7108837dfc092d8a
Nestor <loghim@amazon.com>
2022-06-29 20:22:43 +0300

    primul nostru commit

---
+++ Saptamana1/variabile.py
@@ -0,0 +1,59 @@
+""" Dezvoltarea unui program care sa calculeze venitul saptamanal al unui angajat"""
+""" Venitul saptamanal = venitul/ora * nr ore"""
+""" venit_saptamanal = 35 lei * 40 ore"""
+
+# venit_saptamanal ----> Float
+# venit_pe_ora ---->Float
+# nr ore ----> Float
+
+""" 1. Obtinem venitul ora """
+""" 2. Obtinem nr ore lucrate """
+""" 3. Calcul venit saptamanal """
+""" 4. Afisare rezultate """
+
+venit_pe_ora = float(input("introduceti venitul pe ora: "))
+nr_ore = float(input("introduceti numarul de ora: "))
+venit_saptamanal = venit_pe_ora * nr_ore
+print(" venitul saptamanal este de " + str(venit_saptamanal) + " lei ")
+
+
+# var = "ion"
+# var_2 = 34365.6
+
+# float * float -> float
+# float * integer -> float
+# integer * integer -> integer
+
+
+# 1. numere - float, integer, nr complexe
+# 2. String
+# 3. Liste = [2, 3, 5]
+# 4. Seturi
+# 5. Dictionare
+# 6. Tupluri
+# 7. Boolean
+# 8. Non-type
+
+
+""" operatori de asignare """
+
+# x = 1
+# y = 3
+# x = x +y
+# print(x)
+
+# x = 1
+# y = 3
+# x += Y
+# print(x)
+
+# += / -= / /= / *=
+
+# weekly_income = 10
+# weeklyIncome = 10
+# ex. : and, del, from, not, while, as, return, def = cuv. rezervate
+
+
+# first_number = 1
+# first_Number = first_number + 1
+# print(first_number)
